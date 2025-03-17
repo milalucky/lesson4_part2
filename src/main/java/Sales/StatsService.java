1efdf96692b3c871be1f8a65ad445d2ea8b8e56f
milanazharkevich <tquestufa@gmail.com>
2025-03-17 17:55:48 +0500

    lesson 4 part 2

---
+++ src/main/java/Sales/StatsService.java
@@ -0,0 +1,65 @@
+package Sales;
+
+public class StatsService {
+
+    public int sumSales(int[] sales) { //Сумма всех продаж
+        int sum = 0;
+        for (int sale : sales) {
+            sum = sum + sale;
+        }
+        return sum;
+    }
+
+    public int avgSumSales(int[] sales) { //Средняя сумма продаж
+        int avgSum = sumSales(sales) / sales.length;
+        return avgSum;
+    }
+
+    public int maxMonthSale(int[] sales) { //Месяц с максимальной продажей
+        int max = sales[0];
+        int monthMax = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] > max) {
+                max = sales[i];
+                monthMax = i;
+            }
+        }
+        return monthMax + 1;
+    }
+
+
+    public int minMonthSale(int[] sales) { //Месяц с минимальной продажей
+        int min = sales[0];
+        int monthMin = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] <= min) {
+                min = sales[i];
+                monthMin = i;
+            }
+        }
+        return monthMin + 1;
+    }
+
+    public int countMonthMinSale(int[] sales) { //Кол-во месяцев с продажами ниже среднего
+        int count = 0;
+        int avgSum = avgSumSales(sales);
+        for (int sale : sales) {
+            if (sale < avgSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countMonthMaxSale(int[] sales) { //Кол-во месяцев с продажами выше среднего
+        int count = 0;
+        int avgSum = avgSumSales(sales);
+        for (int sale : sales) {
+            if (sale > avgSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/test/java/TestSales/TestStatsService.java
@@ -0,0 +1,62 @@
+package TestSales;
+
+import Sales.StatsService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestStatsService {
+
+    @Test //Сумма всех продаж
+    public void testSumSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.sumSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //Средняя сумма продаж
+    public void testAvgSumSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.avgSumSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //Месяц с максимальной продажей
+    public void testMaxMonthSale() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 6;
+        int actual = service.maxMonthSale(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //Месяц с минимальной продажей
+    public void testMinMonthSale() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minMonthSale(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //Кол-во месяцев с продажами ниже среднего
+    public void testСountMonthMinSale() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.countMonthMinSale(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //Кол-во месяцев с продажами выше среднего
+    public void testСountMonthMaxSale() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.countMonthMaxSale(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+}
